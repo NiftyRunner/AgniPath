093b576bbd1302e1b44844894a09aadb0b4b45f2
Ansuman-Hemant <ansuman.mitmpl2022@learner.manipal.edu>
2025-01-03 04:24:13 +0530

    Revert "Updated Fire"
    This reverts commit 8bd247b0adf7e97c54221d81a756954232a50cc3.

--- Assets/Scripts/ExtinguishFire.cs
+++ Assets/Scripts/ExtinguishFire.cs
@@ -9,34 +9,30 @@ public class ExtinguishFire : MonoBehaviour
     [SerializeField] GameObject smokePrefab;
     [SerializeField] Vector3 smokeScale;
     [SerializeField] AudioClip extinguishClip;
-    [SerializeField] ParticleSystem newFire;
-    [SerializeField] AudioSource extinguishSource;
+    AudioSource extinguishSource;
     GameObject smokeInstance;
     ParticleSystem smokeParticles;
 
     private void Start()
     {
         isSmokePlayed = false;
-        extinguishSource = GetComponentInChildren<AudioSource>();
-        newFire = GetComponentInChildren<ParticleSystem>();
         fireEffect = GetComponent<VisualEffect>();
+        extinguishSource = GetComponent<AudioSource>();
         smokeInstance = Instantiate(smokePrefab, fireEffect.transform.position, fireEffect.transform.rotation); //If smoke rotation is off check here
         smokeParticles = smokeInstance.GetComponent<ParticleSystem>();
         smokeInstance.transform.localScale = smokeScale;
         fireEffect.Play();
         extinguishSource.Play();
-        newFire.Play();
     }
 
 
     public void ExtingushFire()
     {
-        newFire.Stop();
-        //fireEffect.SetFloat("Smoke Rate", 0f);
-        //fireEffect.SetFloat("Flames Rate", 0f);
-        //fireEffect.SetFloat("Sparks Rate", 0f);
-        //fireEffect.SetFloat("Spark Burst Rate Max", 0f);
-        //fireEffect.SetFloat("Spark Burst Rate Min", 0f);
+        fireEffect.SetFloat("Smoke Rate", 0f);
+        fireEffect.SetFloat("Flames Rate", 0f);
+        fireEffect.SetFloat("Sparks Rate", 0f);
+        fireEffect.SetFloat("Spark Burst Rate Max", 0f);
+        fireEffect.SetFloat("Spark Burst Rate Min", 0f);
         extinguishSource.Stop();
         if (smokeParticles != null && !isSmokePlayed) 
         {
